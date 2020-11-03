f519ff3bf9671459fdad23d8414fc14afc538b57
tedpark27 <prakash.mec27@gmail.com>
2020-11-03 12:05:25 +0530

    Comitting second time for angularCRUD3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import{ RouterModule,Routes } from '@angular/router';
+import{FormsModule} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListEmployeesComponent } from './employees/list-employees.component';
+import { CreateEmployeeComponent } from './employees/create-employee.component';
+
+
+const appRoutes: Routes =[
+  {path: 'list', component: ListEmployeesComponent},
+  {path: 'create', component: CreateEmployeeComponent},
+  {path: '', redirectTo: '/list', pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListEmployeesComponent,
+    
+    CreateEmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    RouterModule,
+    ReactiveFormsModule,
+    CommonModule,
+    RouterModule.forRoot(appRoutes)
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  
+}
---
+++ src/app/employees/create-employee.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import{NgForm} from '@angular/forms';
+import { Compiler } from '@angular/core';
+import{Employee} from '../models/employee.model';
+import{Department} from '../models/department.model';
+import{FormsModule} from '@angular/forms';
+import { ChangeDetectorRef, AfterContentChecked} from '@angular/core';
+
+
+@Component({
+  selector: 'app-create-employee',
+  templateUrl: './create-employee.component.html',
+  styleUrls: ['./create-employee.component.css']
+})
+export class CreateEmployeeComponent implements OnInit {
+  previewPhoto = false;
+  departments:Department[] =[
+    {id:1, name:'help desk'},
+    {id:2, name:'hr'},
+    {id:3, name:'IT'},
+    {id:4, name:'payroll'}
+  ]
+  employee:Employee= {id: null,
+    name: null,
+    gender: null,
+    email: null,
+    phoneNumber: null,
+    contactPreference: null,
+    dateOfBirth: null,
+    department: null,
+    isActive: null,
+    photoPath: null
+  }
+  
+  
+    //constructor(private _compiler: Compiler,) { this._compiler.clearCache();}
+  
+  constructor(
+    private cdref: ChangeDetectorRef) { }
+  
+    ngAfterContentChecked() {
+  
+      this.cdref.detectChanges();
+  
+    }
+  
+ 
+  
+togglePhotoPreview(){
+  this.previewPhoto = !this.previewPhoto;
+}
+  ngOnInit(): void {
+  }
+
+  saveEmployee(newemployee: Employee){
+ console.log(newemployee);
+  }
+
+  id: number;
+    name: string;
+    gender: string;
+    email:string = '';
+    phoneNumber?: number;
+    contactPreference: string;
+    dateOfBirth: Date;
+    department: string;
+    isActive: boolean;
+    photoPath?: string;
+  
+   
+}
